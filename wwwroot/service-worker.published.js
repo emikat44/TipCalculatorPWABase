d82c7b0f692543177d98faded7ceab51450ad88c
emikat44 <Emily.Standard@synergetictech.com>
2024-05-03 11:48:33 -0400

    updated service worker

--- wwwroot/service-worker.published.js
+++ wwwroot/service-worker.published.js
@@ -22,27 +22,27 @@ async function onInstall(event) {
     await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
 }
 
-async function onActivate(event) {
-    console.info('Service worker: Activate');
+//async function onActivate(event) {
+//    console.info('Service worker: Activate');
 
-    // Delete unused caches
-    const cacheKeys = await caches.keys();
-    await Promise.all(cacheKeys
-        .filter(key => key.startsWith(cacheNamePrefix) && key !== cacheName)
-        .map(key => caches.delete(key)));
-}
+//    // Delete unused caches
+//    const cacheKeys = await caches.keys();
+//    await Promise.all(cacheKeys
+//        .filter(key => key.startsWith(cacheNamePrefix) && key !== cacheName)
+//        .map(key => caches.delete(key)));
+//}
 
-async function onFetch(event) {
-    let cachedResponse = null;
-    if (event.request.method === 'GET') {
-        // For all navigation requests, try to serve index.html from cache
-        // If you need some URLs to be server-rendered, edit the following check to exclude those URLs
-        const shouldServeIndexHtml = event.request.mode === 'navigate';
+//async function onFetch(event) {
+//    let cachedResponse = null;
+//    if (event.request.method === 'GET') {
+//        // For all navigation requests, try to serve index.html from cache
+//        // If you need some URLs to be server-rendered, edit the following check to exclude those URLs
+//        const shouldServeIndexHtml = event.request.mode === 'navigate';
 
-        const request = shouldServeIndexHtml ? 'index.html' : event.request;
-        const cache = await caches.open(cacheName);
-        cachedResponse = await cache.match(request);
-    }
+//        const request = shouldServeIndexHtml ? 'index.html' : event.request;
+//        const cache = await caches.open(cacheName);
+//        cachedResponse = await cache.match(request);
+//    }
 
-    return cachedResponse || fetch(event.request);
-}
+//    return cachedResponse || fetch(event.request);
+//}
